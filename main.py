ef46f9c7f5443e9951de85a4352a5786232d3401
Lur3out <aupovaleksandr@gmail.com>
2019-09-16 22:33:29 +0500

    Изменено в порядке создания

--- main.py
+++ main.py
@@ -2,18 +2,6 @@ from datetime import date
 gradebooks = dict()
 card = dict()
 card.update ({
-    "surname" : "Швецов",
-    "name" : "Владислав",
-    "lastname" : "Валерьевич",
-    "birthDate" : date(1997, 4, 29),
-    "faculty" : "Электротехнический",
-    "group" : "АСУ2-19-1м",
-    "course" : 1 })
-
-
-gradebooks.update ({"19-ЭТФ-1257" : card.copy()})
-
-card.update ({
     "surname" : "Аюпов",
     "name" : "Александр",
     "lastname" : "Дамирович",
@@ -23,5 +11,16 @@ card.update ({
     "course" : 1 })
 
 gradebooks.update ({"19-ЭТФ-1248" : card.copy()})
+
+card.update ({
+    "surname" : "Швецов",
+    "name" : "Владислав",
+    "lastname" : "Валерьевич",
+    "birthDate" : date(1997, 4, 29),
+    "faculty" : "Электротехнический",
+    "group" : "АСУ2-19-1м",
+    "course" : 1 })
+
+gradebooks.update ({"19-ЭТФ-1257" : card.copy()})
 
 print (gradebooks)
