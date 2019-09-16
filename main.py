6a4030ed13861d629ce26483e796bf02e8061d2c
Vladislav <shvetsovvlad86@gmail.com>
2019-09-16 17:22:10 +0000

    Got a dict with 2 items

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ card.update ({
     "group" : "АСУ2-19-1м",
     "course" : 1 })
 
-gradebooks.update ({"19-ЭТФ-1257" : card})
+
+gradebooks.update ({"19-ЭТФ-1257" : card.copy()})
+
+print(gradebooks)
+print("/n")
+
+card.update ({
+    "surname" : "Аюпов",
+    "name" : "Александр",
+    "lastname" : "Дамирович",
+    "birthDate" : date(1997, 6, 19),
+    "faculty" : "Электротехнический",
+    "group" : "АСУ2-19-1м",
+    "course" : 1 })
+
+gradebooks.update ({"19-ЭТФ-1248" : card.copy()})
 
 print (gradebooks)
