e1f77202eae2698ccaa633a752c495c4083e9587
Vladislav <shvetsovvlad86@gmail.com>
2019-09-16 16:32:52 +0000

    Added structure of gradebook and info about my person gradebook

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from datetime import date
 gradebooks = dict()
 card = dict()
 card.update ({
-    "surname" : "Аюпов",
-    "name" : "Александр",
-    "lastname" : "Дамирович",
-    "birthDate" : date(1997, 6, 19),
+    "surname" : "Швецов",
+    "name" : "Владислав",
+    "lastname" : "Валерьевич",
+    "birthDate" : date(1997, 4, 29),
     "faculty" : "Электротехнический",
     "group" : "АСУ2-19-1м",
     "course" : 1 })
 
-gradebooks.update ({"19-ЭТФ-1248" : card})
+gradebooks.update ({"19-ЭТФ-1257" : card})
 
 print (gradebooks)
